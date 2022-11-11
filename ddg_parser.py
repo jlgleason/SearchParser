a89a670155ea26be87ad59cf7160269bcdb71880
Alice K <61752564+akoening@users.noreply.github.com>
2022-11-10 19:55:31 -0500

    Add files via upload
    Parser for DDG SERPs

---
+++ ddg_parser.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup, SoupStrainer
+
+import parser
+
+# Treats every component as the same, extracts two types of info:
+# what kind of component (ad or not)
+# and what the url was (to classify the domain)
+# find id=b_results and iterate through descendants
+# API ad client: Fix US region, English sample, match type, ad position
+
+
+# Parses components in results--main tree and also searches for id tags
+def parse_components(soup):
+    parsed_data = []
+    head = soup.body
+    for cmpt in head.find("div", id="links_wrapper"):
+        parsed_data.append(cmpt["class"])
+        class_str = str(cmpt.get("class"))
+        if class_str == "['results--main']":
+            for each in cmpt.children:
+                if each.has_attr("class"):
+                    parsed_data.append(each["class"])
+                elif each.has_attr("id"):
+                    parsed_data.append(each["id"])
+    return parsed_data
+
+
+def main():
+    # Open SERPs file and make soup
+    soup = parser.open_file("/Users/alicekoeninger/PycharmProjects/ddg_searcher/ddg_data.txt")
+    # Parse components
+    parsed_data = parse_components(soup)
+    # Write parsed data to file 
+    parser.save_data("ddg_components.txt", parsed_data)
+
+
+if __name__ == "__main__":
+    main()
