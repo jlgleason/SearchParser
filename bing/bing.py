cc8840571ced651ddf45b4ba43617de3a6a9e551
jlgleason <jeffrey.gleason.3@gmail.com>
2022-12-20 22:46:32 -0500

    refactor bing parser

--- bing/bing.py
+++ bing/bing.py
@@ -6,55 +6,6 @@ from bs4.element import Tag
 
 from .component_parsers import CMPT_PARSERS
 
-# Treats every component as the same, extracts two types of info:
-# what kind of component (ad or not)
-# and what the url was (to classify the domain)
-# find id=b_results and iterate through descendants
-# API ad client: Fix US region, English sample, match type, ad position
-
-
-def open_file(filename: str):
-    try:
-        with open(filename, mode="r") as infile:
-            soup = BeautifulSoup(infile, "lxml")
-            return soup
-    except OSError:
-        print("A file error occured")
-
-
-# Appends new parsed components to components file
-def save_data(newfile: str, parsed_data):
-    with open(newfile, mode="a") as outfile:
-        for each in parsed_data:
-            outfile.write(str(each) + "\n")
-    outfile.close()
-
-
-# Saves prettified data
-def save_pretty(newfile: str, soup):
-    with open(newfile, mode="a") as outfile:
-        for line in soup:
-            outfile.write(str(line))
-    outfile.close()
-
-
-# Parses components in b_results tree and also searches for id tags
-def parse_components(soup):
-    parsed_data = []
-    for cmpt in soup.find("ol", id="b_results"):
-        parsed_data.append(cmpt["class"])
-        if cmpt.has_attr("id"):
-            parsed_data.append(cmpt["id"])
-    return parsed_data
-
-
-def main():
-    # pretty_soup = open_file("bing_data.txt")
-    # save_pretty("bing_output.txt", pretty_soup)
-    soup = open_file("bing_data.txt")
-    parsed_data = parse_components(soup)
-    save_data("b_components.txt", parsed_data)
-
 
 def parse_lang(soup: BeautifulSoup):
     return soup.find("html").attrs["lang"]
@@ -94,7 +45,6 @@ def parse_serp(serp, serp_id: int = None):
     return parsed
 
 
-# Classifies component type
 def classify_type(cmpt: Tag):
     """classifies component type
 
@@ -112,5 +62,29 @@ def classify_type(cmpt: Tag):
         return "unknown"
 
 
-if __name__ == "__main__":
-    main()
+def search(qry: str):
+    """submits a query to Bing using requests
+
+    Args:
+        qry (str): search query
+
+    Returns:
+        bytes: response content
+    """
+
+    return requests.get(
+        f"https://www.bing.com/search",
+        params={
+            "q": qry,
+            "form": "QBLH",
+        },  # need the form param to get a response with ads
+        headers={
+            "Host": "www.bing.com",
+            "Referer": f"https://www.bing.com/",
+            "Accept": "*/*",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0",
+            "Accept-Encoding": "gzip,deflate,br",
+            "Accept-Language": "en-US,en;q=0.5",
+        },
+        timeout=10,
+    ).content
