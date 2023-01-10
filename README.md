2626a97e19ebf5fb1331cfe16dc5f5c770d3a898
jlgleason <jeffrey.gleason.3@gmail.com>
2023-01-10 11:05:49 -0500

    Revert "refactor crawling logic"

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # SearchParser
-Crawl and parse Bing and DuckDuckGo SERPs
+Parse Bing and DuckDuckGo SERPs
 
 ## Usage
 
 ### Test with 1 query: 
 
-``python3 main.py -s bing -fp_qrys "hello world" --test`` 
+``python3 test.py -s bing -q "hello world"``
 
 ### Crawl multiple queries: 
 
 ``python3 main.py -s ddg --fp_qrys qrys.txt --fp_parsed results.json``
 
+## TODO
+- support shopping ads carousel
--- main.py
+++ main.py
@@ -1,87 +1,63 @@
 import argparse
 import asyncio
-import pprint
-import json
-from requests.models import PreparedRequest
+import requests
 
-from pyppeteer import launch, browser
+from pyppeteer import launch
+import tqdm 
 
 from bing import bing
 from ddg import ddg
 from utils import get_new_queries
 
 
-PARSE_FUNCS = {"ddg": ddg.parse_serp, "bing": bing.parse_serp}
-BASE_URLS = {
-    "bing": "https://www.bing.com/search",
-    "ddg": "https://duckduckgo.com",
-}
-PARAMS = {
-    "bing": {"form": "QBLH"},  # need the form param to get a response with ads
-    "ddg": {},
-}
+def run_bing(fp_qrys: str, fp_parsed: str):
+    """runs queries on Bing, saves parsed results
 
+    Args:
+        fp_qrys (str): filepath with line-delimited queries.
+        fp_parsed (str): filepath to save parsed results.
+    """
 
-def build_url(sengine: str, qry: str):
-    """build search url from base, qry, params"""
-    req = PreparedRequest()
-    req.prepare_url(BASE_URLS[sengine], {"q": qry} | PARAMS[sengine])
-    return req.url
-
-
-async def crawl(sengine: str, browser: browser.Browser, qry: str, fp_save: str):
-    """runs a search and parses the results"""
-
-    page = await browser.newPage()
-    user_agent = await page.evaluate("() => navigator.userAgent")
-    await page.setUserAgent(user_agent.replace("HeadlessChrome", "Chrome"))
-    await page.goto(build_url(sengine, qry))
-    html = await page.content()
-    await page.close()
-
-    results = PARSE_FUNCS[sengine](html, qry=qry)
-    if fp_save:
-        for result in results:
-            fp_save.write(json.dumps(result))
-            fp_save.write("\n")
-    else:
-        pp = pprint.PrettyPrinter()
-        pp.pprint(results)
-
-
-async def main(
-    sengine: str,
-    fp_qrys: str,
-    fp_parsed: str = None,
-    n_threads: int = 10,
-    test: bool = False,
-):
-    """runs queries in 'fp_qrys' on 'sengine', saves parsed results to 'fp_parsed'"""
-
-    if test:
-        qrys = [fp_qrys]
-        fp = None
-    else:
-        qrys = get_new_queries(fp_qrys, fp_parsed)
+    qrys = get_new_queries(fp_qrys, fp_parsed)
+    
+    try:
         fp = open(fp_parsed, "a")
+        sesh = requests.Session()
+        for qry in tqdm.tqdm(qrys):
+            bing.crawl(sesh, qry, fp)
+    finally:
+        sesh.close()
+        fp.close()
+
+
+async def run_ddg(fp_qrys: str, fp_parsed: str, n_threads: int = 10):
+    """runs queries on DuckDuckGo, saves parsed results
+
+    Args:
+        fp_qrys (str): filepath with line-delimited queries.
+        fp_parsed (str): filepath to save parsed results.
+        n_threads (int): number of queries to parse in parallel tabs. Defaults to 10.
+    """
+
+    qrys = get_new_queries(fp_qrys, fp_parsed)
 
     try:
+        fp = open(fp_parsed, "a")
         i = 0
         while i <= len(qrys):
+            print(qrys[i:i+n_threads])
             browser = await launch()
             tasks = [
-                asyncio.ensure_future(crawl(sengine, browser, qry, fp))
-                for qry in qrys[i : i + n_threads]
+                asyncio.ensure_future(ddg.crawl(browser, qry, fp))
+                for qry in qrys[i:i+n_threads]
             ]
             await asyncio.gather(*tasks)
             await browser.close()
             i += n_threads
-            num = min(i, len(qrys))
-            print(f"Crawled {num} of {len(qrys)} queries ({round(num, 3)})")
+            print(f'Crawled {i} of {len(qrys)} queries ({round(i/len(qrys), 2)})')
     except:
         await browser.close()
-
-    if not test:
+    finally:
         fp.close()
 
 
@@ -92,30 +68,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s", "--sengine", default="bing", type=str, help="'bing' or 'ddg'"
     )
+    parser.add_argument("--fp_qrys", type=str, help="newline-delimited file of queries")
     parser.add_argument(
-        "-t",
-        "--test",
-        action=argparse.BooleanOptionalAction,
-        help="whether in testing mode",
-        default=False,
+        "--fp_parsed", type=str, help="Save parsing results to this file"
     )
     parser.add_argument(
-        "-q",
-        "--fp_qrys",
-        type=str,
-        help="If not in testing mode, this argument will be treated as a filename for newline-delimited queries. If in testing mode, this argument will be treated as the query string.",
-    )
-    parser.add_argument(
-        "--fp_parsed", type=str, help="Save parsing results to this file", default=None
-    )
-    parser.add_argument(
-        "--n_threads",
-        type=int,
-        help="Number of queries to run in parallel tabs before restarting browser",
-        default=10,
+        "--n_threads", type=int, help="Number of queries to run in parallel for DDG crawling", default=10
     )
     args = parser.parse_args()
 
-    asyncio.run(
-        main(args.sengine, args.fp_qrys, args.fp_parsed, args.n_threads, args.test)
-    )
+    if args.sengine == "bing":
+        run_bing(args.fp_qrys, args.fp_parsed)
+    elif args.sengine == "ddg":
+        asyncio.run(run_ddg(args.fp_qrys, args.fp_parsed, args.n_threads))
